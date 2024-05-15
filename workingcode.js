f5e34b93e53700633b9b32a22580f2eeef7adf04
lopez <lopesdenis@mail.ru>
2024-05-14 21:12:02 -0400

    add cart for pizza

--- workingcode.js
+++ workingcode.js
@@ -1,3 +1,217 @@
+let cartItems = [];
+
+const updateCartIconCount = () => {
+    const cartIcon = document.getElementById('cart-icon');
+    const cartCountElement = cartIcon.querySelector('#cart-count');
+    const cartItemCount = cartItems.reduce((total, item) => total + item.quantity, 0);
+    cartCountElement.textContent = cartItemCount;
+    cartCountElement.style.display = cartItemCount > 0 ? 'inline-block' : 'none';
+};
+
+const renderCart = () => {
+    const cartElement = document.getElementById('cart-items');
+    const totalPriceElement = document.getElementById('total-price');
+    cartElement.innerHTML = '';
+    let totalPrice = 0;
+    cartItems.forEach((item, index) => {
+        const { name, price, quantity, size, toppings } = item;
+        const itemTotal = price * quantity;
+        const itemElement = document.createElement('div');
+        itemElement.classList.add('cart-item');
+        const itemNameElement = document.createElement('span');
+        itemNameElement.textContent = `${name}${size ? ` (${size})` : ''} - ${price} руб. x ${quantity}`;
+        itemNameElement.classList.add('cart-item-name');
+        itemElement.appendChild(itemNameElement);
+
+        if (toppings && toppings.length > 0) {
+            const toppingsElement = document.createElement('div');
+            toppingsElement.textContent = `Ингредиенты: ${toppings.join(', ')}`;
+            itemElement.appendChild(toppingsElement);
+        }
+
+        const removeButton = document.createElement('button');
+        removeButton.textContent = 'Удалить';
+        removeButton.onclick = () => removeFromCart(index);
+        itemElement.appendChild(removeButton);
+
+        const decreaseButton = document.createElement('button');
+        decreaseButton.textContent = '-';
+        decreaseButton.onclick = () => decreaseQuantity(index);
+        itemElement.appendChild(decreaseButton);
+
+        const increaseButton = document.createElement('button');
+        increaseButton.textContent = '+';
+        increaseButton.onclick = () => increaseQuantity(index);
+        itemElement.appendChild(increaseButton);
+
+        cartElement.appendChild(itemElement);
+        totalPrice += itemTotal;
+    });
+    totalPriceElement.textContent = `Общая стоимость: ${totalPrice} руб.`;
+    if (!document.querySelector('.deliveryMessage')) {
+        totalPriceElement.insertAdjacentHTML('afterend', '<p class="deliveryMessage">Стоимость доставки вам сообщит Диспетчер</p>');
+    }
+    updateCartIconCount();
+};
+
+window.onload = () => {
+    const savedCartItems = JSON.parse(localStorage.getItem('cartItems'));
+    if (savedCartItems) {
+        cartItems = savedCartItems;
+    }
+    renderCart();
+};
+
+const addToCart = (name, price, button) => {
+    const card = button.closest('.product');
+    const quantity = parseInt(card.querySelector('.quantity').textContent);
+    const existingItem = cartItems.find(item => item.name === name && !item.size && (!item.toppings || item.toppings.length === 0));
+    if (existingItem) {
+        existingItem.quantity += quantity;
+    } else {
+        cartItems.push({ name, price, quantity });
+    }
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    updateCartIconCount();
+    renderCart();
+};
+
+const addToCartPizza = (name, price, button) => {
+    const card = button.closest('.product');
+    const quantity = parseInt(card.querySelector('.quantity').textContent);
+    const selectedSizeButton = card.querySelector('.size-btn.selected');
+    const selectedSize = selectedSizeButton.dataset.size;
+    const selectedSizePrice = parseInt(selectedSizeButton.dataset.price);
+    const selectedToppings = Array.from(card.querySelectorAll('input[type="checkbox"]:checked')).map(topping => topping.value);
+    const extraPrice = selectedToppings.reduce((total, topping) => total + parseInt(card.querySelector(`input[value="${topping}"]`).dataset.price), 0);
+    const totalPrice = selectedSizePrice + extraPrice;
+    const newItem = { name, price: totalPrice, quantity, size: selectedSize, toppings: selectedToppings };
+    const existingItem = cartItems.find(item => item.name === name && item.size === selectedSize && JSON.stringify(item.toppings) === JSON.stringify(selectedToppings));
+
+    if (existingItem) {
+        existingItem.quantity += quantity;
+    } else {
+        cartItems.push(newItem);
+    }
+
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    updateCartIconCount();
+    renderCart();
+};
+
+const removeFromCart = (index) => {
+    cartItems.splice(index, 1);
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    renderCart();
+    updateCartIconCount();
+};
+
+const decreaseQuantity = (index) => {
+    if (cartItems[index].quantity > 1) {
+        cartItems[index].quantity--;
+        localStorage.setItem('cartItems', JSON.stringify(cartItems));
+        renderCart();
+        updateCartIconCount();
+    }
+};
+
+const increaseQuantity = (index) => {
+    cartItems[index].quantity++;
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    renderCart();
+    updateCartIconCount();
+};
+
+const toggleToppingsMenu = (button) => {
+    const toppingsMenu = button.nextElementSibling;
+    toppingsMenu.style.display = toppingsMenu.style.display === 'none' ? 'block' : 'none';
+};
+
+const selectSize = (button) => {
+    const card = button.closest('.product');
+    const price = button.dataset.price;
+
+    // Устанавливаем новую цену
+    card.querySelector('.myprice').textContent = `Цена: $${price}`;
+    card.querySelector('.total-price').textContent = `Общая стоимость: $${price}`;
+
+    // Убираем класс "selected" у всех кнопок размера
+    card.querySelectorAll('.size-btn').forEach(btn => btn.classList.remove('selected'));
+
+    // Добавляем класс "selected" к текущей кнопке
+    button.classList.add('selected');
+};
+
+
+const decreaseQuantityCard = (button) => {
+    const quantityElement = button.nextElementSibling;
+    let quantity = parseInt(quantityElement.textContent);
+    if (quantity > 1) {
+        quantity--;
+        quantityElement.textContent = quantity;
+    }
+};
+
+const increaseQuantityCard = (button) => {
+    const quantityElement = button.previousElementSibling;
+    let quantity = parseInt(quantityElement.textContent);
+    quantity++;
+    quantityElement.textContent = quantity;
+};
+
+
+
+
+
+
+// конец теста
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Функция для перехода на главную страницу
+const goToMainPage = () => {
+    // Проверяем, находимся ли мы уже на главной странице
+    if (window.location.href !== 'index.html') {
+        // Если нет, перенаправляем пользователя на главную страницу
+        window.location.href = 'index.html';
+    }
+};
+
+
+
+
+
+
+// Функция для проверки корректности введенного имени
+function isValidName(name) {
+    const namePattern = /^[a-zA-Zа-яА-Я ]+$/;
+    return namePattern.test(name);
+}
+
+// Функция для проверки корректности введенного телефона
+function isValidPhone(phone) {
+    const phonePattern = /^\+?\d{10,15}$/;
+    return phonePattern.test(phone);
+}
+
+// Функция для проверки на пустоту полей
+function isNotEmpty(value) {
+    return value && value.trim() !== '';
+}
 
 const checkout = () => { // Определение функции checkout
     const checkoutButton = document.getElementById('checkout-button'); // Получение кнопки оформления заказа
@@ -79,17 +293,22 @@ const checkout = () => { // Определение функции checkout
 
 
 
-
-
 // Обновленная функция отправки заказов, которая теперь принимает параметры
 const sendOrderToTelegram = (name, phone, address) => {
     // Создаем сообщение с данными о заказе и информацией о клиенте
     let message = `Новый заказ!\n\nИмя: ${name}\nТелефон: ${phone}\nАдрес: ${address}\n\n`;
     let totalPrice = calculateTotalPrice();
 
-    cartItems.forEach(({ name, price, quantity }) => {
+    cartItems.forEach(({ name, price, quantity, size, toppings }) => {
         const itemTotal = price * quantity;
-        message += `${name} - ${price} руб. x ${quantity} = ${itemTotal} руб.\n`;
+        message += `${name} - ${price} руб. x ${quantity} = ${itemTotal} руб.`;
+        if (size) {
+            message += ` (${size})`;
+        }
+        if (toppings && toppings.length > 0) {
+            message += `\nДополнительные ингредиенты: ${toppings.join(', ')}`;
+        }
+        message += '\n';
     });
 
     message += `\nОбщая стоимость: ${totalPrice} руб.`;
